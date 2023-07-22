df7a007e010c242f52576893eda9bfc75094a712
Victor Dominguez <vdomim@gmail.com>
2023-07-22 02:34:12 +0200

    Uploading exercises 3.2

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.get('/', (req, res) => {
     res.send('<h1>Phonebook application</h1>')
 })
 
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`
+    )
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })

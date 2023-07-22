58c1ec3ec7a9461a695d509bcc7d9ee6dbcc2b7b
Victor Dominguez <vdomim@gmail.com>
2023-07-22 04:06:55 +0200

    Uploading exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -84,7 +86,7 @@ app.post('/api/persons/', (req, res) => {
             error: "Name must be unique"
         })
     }
-    
+
     const person = {
         name: body.name,
         number: body.number,
